38e3b55d6053d07ee75470a533c93b34151089eb
Maestro CI <maestro@snapdragonpartners.com>
2026-03-10 20:52:14 -0500

    Brew cask update for maestro version v1.3.5

--- Casks/maestro.rb
+++ Casks/maestro.rb
@@ -3,7 +3,7 @@ cask "maestro" do
   name "maestro"
   desc "Multi-agent AI coding system orchestrator"
   homepage "https://github.com/SnapdragonPartners/maestro"
-  version "1.3.4"
+  version "1.3.5"
 
   livecheck do
     skip "Auto-generated on release."
@@ -14,22 +14,22 @@ cask "maestro" do
   on_macos do
     on_intel do
       url "https://github.com/SnapdragonPartners/maestro/releases/download/v#{version}/maestro_#{version}_darwin_amd64.zip"
-      sha256 "8272e957872313d3785a8a31060e18c89121bdcc1c39601a5c7655dad8cc462a"
+      sha256 "e6e5e1d0abf9deddbe0f30b069a750631594e9cf7bfd17a4840d253d58b77bad"
     end
     on_arm do
       url "https://github.com/SnapdragonPartners/maestro/releases/download/v#{version}/maestro_#{version}_darwin_arm64.zip"
-      sha256 "62d18c747316ee98981d7111e1956be09e91125a8942929af9ba4ddf69e36ac9"
+      sha256 "feaaa3b48de68c4b0b11677317dfcdfcfbcbcb567cb3a2e5c733bfa81a416245"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/SnapdragonPartners/maestro/releases/download/v#{version}/maestro_#{version}_linux_amd64.tar.gz"
-      sha256 "c7a149efbcfb659834ef7075ff713cc64ec2ea00a9b4262bb803643bc3b3fa0c"
+      sha256 "ee0c175c87c0b1a09985b2ad53be6330c8552c9275275f341bfecbd3dbfaebe6"
     end
     on_arm do
       url "https://github.com/SnapdragonPartners/maestro/releases/download/v#{version}/maestro_#{version}_linux_arm64.tar.gz"
-      sha256 "3445fb1b3e6c127c65912cfd8961b52563a1fc9ef7e65bd6e267623a41b03bec"
+      sha256 "77712637fa197c66879ca31e2fdabc4028c9204b585af7b0473c233d841f5e9a"
     end
   end
 
